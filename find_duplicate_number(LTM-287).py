a644d36149a0a63bb89eeed6f7de360de71cc8fc
Abhinav <er.abhi01@gmail.com>
2020-09-04 19:06:24 +0530

    3 solutions

---
+++ find_duplicate_number(LTM-287).py
@@ -0,0 +1,59 @@
+"""
+Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
+
+There is only one duplicate number in nums, return this duplicate number.
+
+Follow-ups:
+
+How can we prove that at least one duplicate number must exist in nums?
+Can you solve the problem without modifying the array nums?
+Can you solve the problem using only constant, O(1) extra space?
+Can you solve the problem with runtime complexity less than O(n2)?
+
+
+Example 1:
+
+Input: nums = [1,3,4,2,2]
+Output: 2
+Example 2:
+
+Input: nums = [3,1,3,4,2]
+Output: 3
+Example 3:
+
+Input: nums = [1,1]
+Output: 1
+"""
+
+
+class Solution:
+    def findDuplicate1(self, nums) -> int:
+        # Find the intersection point of the two runners.
+        tortoise = hare = nums[0]
+        while True:
+            tortoise = nums[tortoise]
+            hare = nums[nums[hare]]
+            if tortoise == hare:
+                break
+
+        # Find the "entrance" to the cycle.
+        tortoise = nums[0]
+        while tortoise != hare:
+            tortoise = nums[tortoise]
+            hare = nums[hare]
+
+        return hare
+
+    def findDuplicate2(self, nums) -> int:
+        nums.sort()
+        for i in range(1, len(nums)):
+            if nums[i] == nums[i-1]:
+                return nums[i]
+
+    def findDuplicate3(self, nums) -> int:
+        myset = set()
+        for i in nums:
+            if i in myset:
+                return i
+            else:
+                myset.add(i)
