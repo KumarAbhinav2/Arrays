508f76b1fb502f9d6a2e4958c4304b294ae7a792
Abhinav <er.abhi01@gmail.com>
2020-09-04 18:34:03 +0530

    adds one more solution

--- evaluate_reverse_polish_notation(LTM-150).py
+++ evaluate_reverse_polish_notation(LTM-150).py
@@ -55,6 +55,25 @@ class Solution:
                     continue
         return st.pop()
 
+    def evalRPN2(self, tokens) -> int:
+        stack = []
+        operators = {'+': lambda x, y: x + y,
+                     '-': lambda x, y: x - y,
+                     '*': lambda x, y: x * y,
+                     '/': lambda x, y: int(x / y)}
+        for val in tokens:
+            if val in operators:
+                # print (stack)
+                y = int(stack.pop())
+                x = int(stack.pop())
+
+                result = operators[val](x, y)
+                stack.append(result)
+            else:
+                stack.append(val)
+
+        return stack.pop()
+
 obj = Solution()
 #print(obj.evalRPN(["4", "13", "5", "/", "+"]))
 print(obj.evalRPN(["10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+"]))
