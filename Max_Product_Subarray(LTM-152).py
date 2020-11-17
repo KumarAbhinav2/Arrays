c3a1318622317e65ca1239f136711528482306e0
Abhinav <er.abhi01@gmail.com>
2020-11-17 10:42:02 +0530

    adds good one

---
+++ Max_Product_Subarray(LTM-152).py
@@ -0,0 +1,35 @@
+"""
+Given an integer array nums, find the contiguous subarray within an array (containing at least one number)
+which has the largest product.
+
+Example 1:
+
+Input: [2,3,-2,4]
+Output: 6
+Explanation: [2,3] has the largest product 6.
+Example 2:
+
+Input: [-2,0,-1]
+Output: 0
+Explanation: The result cannot be 2, because [-2,-1] is not a subarray.
+"""
+
+
+class Solution:
+    def maxProduct(self, nums) -> int:
+        if len(nums) == 0:
+            return 0
+
+        max_so_far = nums[0]
+        min_so_far = nums[0]
+        result = max_so_far
+
+        for i in range(1, len(nums)):
+            temp_arr = max(nums[i], nums[i] * max_so_far, nums[i] * min_so_far)
+            min_so_far = min(nums[i], nums[i] * max_so_far, nums[i] * min_so_far)
+
+            max_so_far = temp_arr
+
+            result = max(result, max_so_far)
+
+        return result
